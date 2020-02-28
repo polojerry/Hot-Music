9a1b9b5f7b5434bb14b3b136c4d81b57cbce89b1
polojerry <polojerry4@gmail.com>
2020-02-28 12:31:51 +0300

    Initial Project Setup

---
+++ app/src/main/java/com/polotechnologies/hotmusic/uiHost/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.polotechnologies.hotmusic.uiHost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.polotechnologies.hotmusic.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

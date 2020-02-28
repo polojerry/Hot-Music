dccd6266ed8330d7e42710b3b5ebd2bba938a498
polojerry <polojerry4@gmail.com>
2020-02-28 17:22:57 +0300

    Added bottom Nav navigation

--- app/src/main/java/com/polotechnologies/hotmusic/uiHost/HomeFragment.kt
+++ app/src/main/java/com/polotechnologies/hotmusic/uiHost/HomeFragment.kt
@@ -5,19 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.polotechnologies.hotmusic.R
+import com.polotechnologies.hotmusic.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
 
+    private lateinit var mBinding : FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+
+        setupBottomNavigation()
+        return mBinding.root
+    }
+
+    private fun setupBottomNavigation() {
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.nav_host_home) as NavHostFragment
+
+        mBinding.bottomNavMain
+            .setupWithNavController(navController = navHostFragment.navController)
     }
 
 }
